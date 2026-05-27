8c2243f5e258d20629bd0e3b5b60b56b1d15ecaa
Tshegofatso <135760966+Tshegofatso3@users.noreply.github.com>
2026-05-27 10:54:19 +0200

    Test3

--- NoticeBoardApi/Program.cs
+++ NoticeBoardApi/Program.cs
@@ -79,7 +79,7 @@ if (app.Environment.IsDevelopment())
 
     });
 }
-//ygyy
+//ygyyhuh
 app.UseHttpsRedirection();
 app.UseMiddleware<RequestLoggingMiddleware>();
 using (var scope = app.Services.CreateScope())
